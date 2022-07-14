05ba1edc943783be9d2fb6565359108788d3d307
SHIVSB <2020kuec2005@iiitkota.ac.in>
2022-07-14 06:27:47 +0530

    initialising swagger documentation for enterios

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import logger from "morgan";
+import cors from "cors";
+const PORT = process.env.PORT || 5000;
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Enterios swagger documentation",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:7071/api/",
+        description: "Development server",
+      },
+      // {
+      //   url: "yet to be published",
+      //   description: "Production Server",
+      // },
+    ],
+  },
+  apis: ["swagger.yml"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const app: Application = express();
+
+app.use(logger("dev"));
+app.use(cors({ origin: '*'}));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get("/", (req: Request, res: Response) => {
+  res
+    .status(200)
+    .send(
+      "This is the Enterios-Severless Swagger documentation server. Happy Hacking!"
+    );
+});
+
+app.listen(PORT, () => {
+  console.log(
+    "This is the Enterios-Severless Swagger documentation server. Happy Hacking!"
+  );
+  console.log(`Server running on port : ${PORT}`);
+});
